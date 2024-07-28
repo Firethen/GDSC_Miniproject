abd34149abf18e7f57ec60640a893d5529403c5d
2020077329 <knh999nh@naver.com>
2024-07-28 23:55:51 +0900

    /product-details : product_name 반환하도록 추가

--- app/product.py
+++ app/product.py
@@ -52,6 +52,10 @@ def get_product_details():
     p_like = False
     m_like = True
     
+    #상품 이름(타이틀) 찾기
+    pr = Product.query.filter_by(id = product_id).first()
+    product_name = pr.name
+
     #공구상품id, 가격 찾기
     gonggu_product = Gonggu_product.query.filter_by(market_id=market_id,product_id = product_id).first()
     gonggu_product_id = gonggu_product.id
@@ -67,7 +71,6 @@ def get_product_details():
         }
         group_list.append(group_data)
         
-
     #마켓이름 찾기
     market = Market.query.filter_by(id=market_id).first()
     market_name = market.name
@@ -92,6 +95,7 @@ def get_product_details():
             'product_id': product_id,
             'market_id': market_id,
             'market_name': market_name,
+            'product_name' : product_name,
             'product_like': p_like,             #상품 찜 여부(True,False로 나타냄)
             'market_like': m_like,              #마켓 찜 여부
             'keyword_names': keyword_names,      # 키워드 이름이 리스트형태
